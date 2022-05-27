e6d0bb0df7c4fa16050d67e333e56d5147088e66
boshoff7 <boshoff7@gmail.com>
2022-05-27 15:56:59 +0200

    Initial commit

--- Roll'em/ViewController.swift
+++ Roll'em/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // to link image to code press ctrl and drag image
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageVeiw2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageVeiw2.image = diceArray.randomElement()
+    
     }
-
-
+    
 }
-
